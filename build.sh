b6ffd416bd4be81cb08b4757395e7c939fa4be31
Zheng-Bote <zheng.bote@googlemail.com>
2026-06-21 11:57:02 +0200

    dev: lazy build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 MITM_VERSION=$(git describe --tags)
 
-go build -ldflags "-X main.version=${MITM_VERSION}" -o ./bin/mitm-deliver ./cmd/deliver/main.go
+CGO_ENABLED=0 go build -ldflags="-s -w -X main.version=${MITM_VERSION}" -o ./bin/mitm-deliver ./cmd/deliver/main.go
 
 cp bin/mitm-deliver ../../scheduler/mitm_scheduler/bin/.
